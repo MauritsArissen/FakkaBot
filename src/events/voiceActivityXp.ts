8fa418e2e71f8c0f53b582a24b681ec02ebf6eba
Maurits Arissen <43166413+MauritsArissen@users.noreply.github.com>
2022-10-02 20:58:33 +0200

    Change xp formula vc

--- src/events/voiceActivityXp.ts
+++ src/events/voiceActivityXp.ts
@@ -11,7 +11,7 @@ export default {
     const guilds = await client.guilds.cache;
     guilds.forEach(async guild => {
         const members = await guild.members.cache.filter(member => member.voice.channel != null && member.voice.channelId != guild.afkChannelId);
-        members.forEach(m => LevelHelper.addXp(m.id, Math.round(Math.random()*15+15), 2 * 60 * 1000));
+        members.forEach(m => LevelHelper.addXp(m.id, Math.round(Math.random()*10+20), 2 * 60 * 1000));
     });
     setTimeout(() => this.loop(client), 10 * 1000);
   }
