75f1ae2fc681ab720fa7f6e475ab646c19aae626
Maurits Arissen <43166413+MauritsArissen@users.noreply.github.com>
2022-10-03 23:33:14 +0200

    Update voiceActivityXp.ts

--- src/events/voiceActivityXp.ts
+++ src/events/voiceActivityXp.ts
@@ -10,7 +10,10 @@ export default {
   async loop(client: Bot) {
     const guilds = await client.guilds.cache;
     guilds.forEach(async guild => {
-        const members = await guild.members.cache.filter(member => member.voice.channel != null && member.voice.channelId != guild.afkChannelId);
+        const members = await guild.members.cache.filter(member => member.voice.channel != null && 
+                                                         member.voice.channelId != guild.afkChannelId &&
+                                                         member.voice.channel.members.size > 1 &&
+                                                         member.voice.selfMute == false);
         members.forEach(m => LevelHelper.addXp(m.id, Math.round(Math.random()*10+20), 2 * 60));
     });
     setTimeout(() => this.loop(client), 10 * 1000);
