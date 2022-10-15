5f2facb2e355382a0b15e79e5889aa575014865a
Maurits Arissen <marissen1@live.nl>
2022-10-15 23:01:05 +0200

    Made accessible top command

--- src/interactions/commands/top.ts
+++ src/interactions/commands/top.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { ActionRowBuilder, APISelectMenuOption, CommandInteraction, SelectMenuBuilder, SelectMenuOptionBuilder, SlashCommandBuilder } from "discord.js";
 import LevelHelper from "../../util/LevelHelper";
 import UserStats from "../../models/UserStats.model";
 
@@ -7,28 +7,27 @@ export default {
         .setName("top")
         .setDescription("Show leaderboard"),
     async execute(interaction: CommandInteraction) {
-        if (interaction.user.id != '244909794836611082') return interaction.deferReply();
+        const stats: UserStats[] = await UserStats.findAll({ order: [['xp', 'DESC']], limit: 15 });
+        const users = await interaction.guild.members.fetch({ user: stats.map(x => x.uid)});
 
-        setTimeout(() => loop(interaction), 1000);
+        const selectMenu = new SelectMenuBuilder()
+            .setPlaceholder("Click to open!")
+            .setCustomId("Blabla")
+            .addOptions(stats.map(x => {
+                return new SelectMenuOptionBuilder()
+                    .setLabel(`${users.get(x.uid).displayName}`)
+                    .setDescription(`Level: ${LevelHelper.getLvl(x)} | Xp: ${LevelHelper.getCurrentXp(x)}/${LevelHelper.getRequiredXp(x)} | Total xp: ${x.xp}`)
+                    .setValue(`${x.uid}`)
+                    .setEmoji('🔸')
+            }));
         
-        interaction.reply({
-            content: `Sent!`
+        const row = new ActionRowBuilder<SelectMenuBuilder>()
+            .addComponents(selectMenu);
+        
+        await interaction.reply({
+            content: `📢 **Fakka Top 15**\n*Click on the select menu to open the leaderboard, ordered the best 15 from top to bottom*`,
+            components: [row]
         })
+        
     }
-}
-
-const loop = async (interaction: CommandInteraction) => {
-    const stats: UserStats[] = await UserStats.findAll({ order: [['xp', 'DESC']], limit: 10 });
-    const users = await interaction.guild.members.fetch({ user: stats.map(x => x.uid)});
-    const data = stats.map(x => { 
-        return { 
-            uid: users.get(x.uid).displayName, 
-            currentXp: LevelHelper.getCurrentXp(x),
-            remainingXp: LevelHelper.getRequiredXp(x),
-            level: LevelHelper.getLvl(x)
-        }
-    });
-    const dataString = data.map(x => `[ ${x.uid} ]\nLevel: ${x.level} | Xp: ${x.currentXp} / ${x.remainingXp}`).join("\n\n");
-    interaction.editReply(`\`\`\`ini\n${dataString}\n\`\`\``)
-    setTimeout(() => loop(interaction), 20000);
 }
