1a5a026fb12b92658d23e984a9d39bced289a0fd
Maurits Arissen <marissen1@live.nl>
2022-10-16 11:53:08 +0200

    Updated leaderboard

--- src/interactions/commands/top.ts
+++ src/interactions/commands/top.ts
@@ -1,33 +1,44 @@
-import { ActionRowBuilder, CommandInteraction, SelectMenuBuilder, SelectMenuOptionBuilder, SlashCommandBuilder } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, CommandInteraction, SelectMenuBuilder, SelectMenuOptionBuilder, SlashCommandBuilder } from "discord.js";
 import LevelHelper from "../../util/LevelHelper";
 import UserStats from "../../models/UserStats.model";
+import emojis from "../../config/emojis";
 
 export default {
     data: new SlashCommandBuilder()
         .setName("top")
         .setDescription("Show leaderboard"),
     async execute(interaction: CommandInteraction) {
-        const stats: UserStats[] = await UserStats.findAll({ order: [['xp', 'DESC']], limit: 15 });
+        const stats: UserStats[] = await UserStats.findAll({ order: [['xp', 'DESC']], limit: 40 });
         const users = await interaction.guild.members.fetch({ user: stats.map(x => x.uid)});
 
         const selectMenu = new SelectMenuBuilder()
             .setPlaceholder("Click to open!")
             .setCustomId("Blabla")
-            .addOptions(stats.map(x => {
-                return new SelectMenuOptionBuilder()
-                    .setLabel(`${users.get(x.uid).displayName}`)
-                    .setDescription(`Level: ${LevelHelper.getLvl(x)} | Xp: ${LevelHelper.getCurrentXp(x)}/${LevelHelper.getRequiredXp(x)} | Total xp: ${x.xp}`)
-                    .setValue(`${x.uid}`)
-                    .setEmoji('🔸')
-            }));
+
+        for (let i = 0; i < stats.length; i++) {
+            selectMenu.addOptions(new SelectMenuOptionBuilder()
+                .setLabel(`${users.get(stats[i].uid).displayName}`)
+                .setDescription(`Level: ${LevelHelper.getLvl(stats[i])} | Xp: ${LevelHelper.getCurrentXp(stats[i])}/${LevelHelper.getRequiredXp(stats[i])} | Total xp: ${stats[i].xp}`)
+                .setValue(`${stats[i].uid}`)
+                .setEmoji({ id: emojis[`${i+1}`] })
+            );
+        }
         
+        const button = new ButtonBuilder()
+            .setLabel("Refresh")
+            .setStyle(ButtonStyle.Secondary)
+            .setCustomId("refreshTop");
+            
         const row = new ActionRowBuilder<SelectMenuBuilder>()
             .addComponents(selectMenu);
+
+        const row2 = new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(button)
         
         await interaction.reply({
-            content: `📢 **Fakka Top 15**\n*Click on the select menu to open the leaderboard, ordered the best 15 from top to bottom*`,
-            components: [row]
-        })
+            content: `> 📢 **The Fakka Leaderboards** 📢\n\nLast updated: <t:${Math.floor(Date.now()/1000)}:R>`,
+            components: [row, row2]
+        });
         
     }
 }
