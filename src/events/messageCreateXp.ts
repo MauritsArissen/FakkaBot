1a84dfa17b42fb41cd8c4a041632a75067242729
Maurits Arissen <marissen1@live.nl>
2022-12-29 16:43:01 +0100

    👮🏼 Remove double checking xpCooldown

--- src/events/messageCreateXp.ts
+++ src/events/messageCreateXp.ts
@@ -1,28 +1,12 @@
-import { yellow } from "colorette";
 import { Message } from "discord.js";
-import Container from "typedi";
-import { Bot } from "../client";
 import LevelHelper from "../util/LevelHelper";
-import Logger from "../util/Logger";
 
 export default {
   name: "messageCreate",
   once: false,
   async execute(message: Message) {
     if (message.author.bot) return;
-    const client: Bot = Container.get("client");
-    if (
-      !client.xpCooldown.has(message.author.id) ||
-      client.xpCooldown.get(message.author.id) < Date.now()
-    ) {
-      const xp = Math.round(Math.random() * 10 + 15);
-      Logger.log(
-        `MessageCreateXp check event: Adding ${yellow(xp + " xp")} to ${yellow(
-          message.author.tag
-        )}`,
-        "EVENT"
-      );
-      LevelHelper.addXp(message.author.id, xp, 60);
-    }
+    const xp = Math.round(Math.random() * 10 + 15);
+    LevelHelper.addXp(message.author.id, xp, 60);
   },
 };
