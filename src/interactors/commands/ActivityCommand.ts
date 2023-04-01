083ef2abc7f214b2c78a139e2c020e25d6b9ea40
Maurits Arissen <43166413+MauritsArissen@users.noreply.github.com>
2023-04-01 15:40:17 +0200

    🛼Activity roles event (#35)

--- src/interactors/commands/ActivityCommand.ts
+++ src/interactors/commands/ActivityCommand.ts
@@ -1,12 +1,17 @@
 import { PrismaClient } from "@prisma/client";
-import { SlashCommandBuilder, CommandInteraction } from "discord.js";
+import {
+  SlashCommandBuilder,
+  CommandInteraction,
+  EmbedBuilder,
+} from "discord.js";
 import { autoInjectable } from "tsyringe";
 import ICommand from "../../interfaces/ICommand";
 import TimeHelper from "../../util/TimeHelper";
+import { Bot } from "../../client";
 
 @autoInjectable()
 class ActivityCommand implements ICommand {
-  constructor(private prisma?: PrismaClient) {}
+  constructor(private prisma?: PrismaClient, private client?: Bot) {}
 
   getName(): string {
     return "activity";
@@ -26,6 +31,8 @@ class ActivityCommand implements ICommand {
   }
 
   async execute(interaction: CommandInteraction): Promise<any> {
+    const botUsr = await interaction.guild.members.fetch(this.client.user.id);
+
     const userActivity = await this.prisma.activity.aggregate({
       where: {
         uid: interaction.user.id,
@@ -34,11 +41,26 @@ class ActivityCommand implements ICommand {
       _sum: { level: true },
     });
 
-    interaction.reply({
-      content: `Your activity over the past 14 days was a total of: **${TimeHelper.minuteToTimeFormat(
-        userActivity._sum.level
-      )}**`,
-      ephemeral: true,
+    const embed = new EmbedBuilder()
+      .setColor(botUsr.displayHexColor)
+      .setDescription("Your activity tracker")
+      .addFields(
+        {
+          name: "Past 14 days",
+          value: TimeHelper.minuteToTimeFormat(
+            userActivity._sum.level
+          ).toString(),
+        },
+        {
+          name: "Average per day",
+          value: TimeHelper.minuteToTimeFormat(
+            userActivity._sum.level / 14
+          ).toString(),
+        }
+      );
+
+    await interaction.reply({
+      embeds: [embed],
     });
   }
 }
